11da4d3cb5ce4a5edd82e8d09f341769a9cb2ae5
uchenik753 <150526397+uchenik753@users.noreply.github.com>
2025-02-02 14:44:58 +0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,5 @@
+document.getElementById('contact-form').addEventListener('submit', function(event) {
+    event.preventDefault();
+    alert('Ваша заявка отправлена! Мы свяжемся с вами в ближайшее время.');
+    // Здесь можно добавить код для отправки данных на сервер
+});
